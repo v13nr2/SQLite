0cf066a43bec24b58596d9839f4c289c2aa95773
Nanang Rustianto <nanang.programmer@gmail.com>
2021-03-05 03:55:02 +0800

    prepare add act

---
+++ app/src/main/java/com/jogjaide/ternakapp/anak/AddAnakActivity.java
@@ -0,0 +1,15 @@
+package com.jogjaide.ternakapp.anak;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.jogjaide.ternakapp.R;
+
+
+public class AddAnakActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_anak);
+    }
+}
---
+++ app/src/main/java/com/jogjaide/ternakapp/induk/AddIndukActivity.java
@@ -0,0 +1,14 @@
+package com.jogjaide.ternakapp.induk;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.jogjaide.ternakapp.R;
+
+public class AddIndukActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_induk);
+    }
+}
